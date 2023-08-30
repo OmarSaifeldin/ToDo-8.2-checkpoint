a6167f5bf3a522d476e24a3acd8f7dcd502e7946
Omar Saifeldin <omarsaifeldin252@@gmail.com>
2023-08-30 09:35:52 +0400

    Initial Commit

---
+++ ToDo 8.2 checkpoint/ToDo_8_2_checkpointApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_8_2_checkpointApp.swift
+//  ToDo 8.2 checkpoint
+//
+//  Created by Omar Saifeldin on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_8_2_checkpointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
